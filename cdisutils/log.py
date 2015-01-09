6c3138bb946da6b68f860ed495f2889517a3b565
James Porter <porterjamesj@gmail.com>
2015-01-09 13:51:36 -0600

    tweaks to logging

--- cdisutils/log.py
+++ cdisutils/log.py
@@ -3,14 +3,20 @@
 import logging
 import sys
 
+LOGGERS = {}
+
 
 def get_logger(name):
     """Return an opinionated basic logger named `name` that logs to
     stdout."""
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)-4s: %(message)s')
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if LOGGERS.get(name):
+        return LOGGERS.get(name)
+    else:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.propagate = False
     return logger
