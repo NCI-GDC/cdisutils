64f031ff0057138966e02105aea0634778ce5d5e
Joshua Miller <jshuasmiller@gmail.com>
2015-02-23 18:26:21 +0000

    add process pool with better sigint handling

---
+++ cdisutils/pool.py
@@ -0,0 +1,28 @@
+from multiprocessing.pool import Pool
+
+
+def split(self, a, n):
+    """Split a into n evenly sized chunks"""
+    k, m = len(a) / n, len(a) % n
+    chunks = [a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)]
+              for i in xrange(n)]
+    assert sum([len(c) for c in chunks]) == len(a)
+    return chunks
+
+
+class ProcessPool(object):
+
+    def __init__(self, processes):
+        self.processes = processes
+
+    def map(self, func, sequence):
+        pool = Pool(self.processes)
+        try:
+            results = pool.map_async(func, sequence).get(999999)
+        except KeyboardInterrupt:
+            pool.close()
+            pool.terminate()
+            return
+
+        pool.close()
+        return results
