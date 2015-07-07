7b81a7cbcd58629fa1b61ded464ff04a0c3587c1
Phillis Tang <phillis.tt@gmail.com>
2015-07-07 21:46:41 +0000

    Add handler function

--- cdisutils/log.py
+++ cdisutils/log.py
@@ -6,6 +6,14 @@ import sys
 LOGGERS = {}
 
 
+def get_handler():
+    """Return a stdout stream handler"""
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
+    handler.setFormatter(formatter)
+    return handler
+
+
 def get_logger(name):
     """Return an opinionated basic logger named `name` that logs to
     stdout."""
@@ -14,11 +22,8 @@ def get_logger(name):
     else:
         logger = logging.getLogger(name)
         logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
         if not (len(logger.handlers) > 0
                 and type(logger.handlers[0]) == logging.StreamHandler):
-            handler = logging.StreamHandler(sys.stdout)
-            handler.setFormatter(formatter)
-            logger.addHandler(handler)
-        logger.propagate = False
+            logger.addHandler(get_handler())
+            logger.propagate = False
     return logger
