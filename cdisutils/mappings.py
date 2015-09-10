4a75cc05c7ba2174e70cca9c9ea7e93947f7a868
Phillis Tang <phillis.tt@gmail.com>
2015-09-10 16:38:00 +0000

    feat(mapping): add CGCI program

--- cdisutils/mappings.py
+++ cdisutils/mappings.py
@@ -4,19 +4,28 @@ class Mapping(object):
         'phs000178': [],
         'phs000218': ['phs000463', 'phs000464', 'phs000465', 'phs000515',
                       'phs000471', 'phs000466', 'phs000470', 'phs000467',
-                      'phs000468', 'phs000469']
+                      'phs000468', 'phs000469'],
+        'phs000235': ['phs000527', 'phs000528', 'phs000529', 'phs000530',
+                      'phs000531', 'phs000532', 'phs000533']
     }
 
     PHSID_TO_PROJECT = {
         'phs000178': {
-            'TCGA-LAML', 'TCGA-ACC', 'TCGA-BLCA', 'TCGA-LGG', 'TCGA-BRCA', 'TCGA-CESC', 'TCGA-CHOL', 'TCGA-COAD', 'TCGA-ESCA',
-            'TCGA-GBM', 'TCGA-HNSC', 'TCGA-KICH', 'TCGA-KIRC', 'TCGA-KIRP', 'TCGA-LIHC', 'TCGA-LUAD', 'TCGA-LUSC', 'TCGA-DLBC',
-            'TCGA-MESO', 'TCGA-OV', 'TCGA-PAAD', 'TCGA-PCPG', 'TCGA-PRAD', 'TCGA-READ', 'TCGA-SARC', 'TCGA-SKCM', 'TCGA-STAD',
-            'TCGA-TGCT', 'TCGA-THYM', 'TCGA-THCA', 'TCGA-UCS', 'TCGA-UCEC', 'TCGA-UVM', 'TCGA-MISC', 'TCGA-LCML', 'TCGA-FPPP',
-            'TCGA-CNTL'},
+            'TCGA-LAML', 'TCGA-ACC', 'TCGA-BLCA', 'TCGA-LGG', 'TCGA-BRCA',
+            'TCGA-CESC', 'TCGA-CHOL', 'TCGA-COAD', 'TCGA-ESCA',
+            'TCGA-GBM', 'TCGA-HNSC', 'TCGA-KICH', 'TCGA-KIRC', 'TCGA-KIRP',
+            'TCGA-LIHC', 'TCGA-LUAD', 'TCGA-LUSC', 'TCGA-DLBC',
+            'TCGA-MESO', 'TCGA-OV', 'TCGA-PAAD', 'TCGA-PCPG',
+            'TCGA-PRAD', 'TCGA-READ', 'TCGA-SARC', 'TCGA-SKCM', 'TCGA-STAD',
+            'TCGA-TGCT', 'TCGA-THYM', 'TCGA-THCA', 'TCGA-UCS', 'TCGA-UCEC',
+            'TCGA-UVM', 'TCGA-MISC', 'TCGA-LCML', 'TCGA-FPPP', 'TCGA-CNTL'},
         'phs000218': [
-            'TARGET-ALL-P1', 'TARGET-ALL-P2', 'TARGET-AML', 'TARGET-AML-IF', 'TARGET-WT', 'TARGET-CCSK', 'TARGET-RT', 'TARGET-NBL', 'TARGET-OS',
+            'TARGET-ALL-P1', 'TARGET-ALL-P2', 'TARGET-AML', 'TARGET-AML-IF',
+            'TARGET-WT', 'TARGET-CCSK', 'TARGET-RT', 'TARGET-NBL', 'TARGET-OS',
             'TARGET-MDLS'],
+        'phs000235': [
+            'CGCI-BLGSP', 'CGCI-HTMCP-CC', 'CGCI-HTMCP-DLBCL',
+            'CGCI-HTMCP-LC', 'CGCI-MB', 'CGCI-NHL-DLBCL', 'CGCI-NHL-FL'],
         'phs000463': ['TARGET-ALL-P1'],
         'phs000464': ['TARGET-ALL-P2'],
         'phs000465': ['TARGET-AML'],
@@ -26,7 +35,14 @@ class Mapping(object):
         'phs000470': ['TARGET-RT'],
         'phs000467': ['TARGET-NBL'],
         'phs000468': ['TARGET-OS'],
-        'phs000469': ['TARGET-MDLS']
+        'phs000469': ['TARGET-MDLS'],
+        'phs000527': ['CGCI-BLGSP'],
+        'phs000528': ['CGCI-HTMCP-CC'],
+        'phs000529': ['CGCI-HTMCP-DLBCL'],
+        'phs000530': ['CGCI-HTMCP-LC'],
+        'phs000531': ['CGCI-MB'],
+        'phs000532': ['CGCI-NHL-DLBCL'],
+        'phs000533': ['CGCI-NHL-FL'],
     }
 
     PROJECT_TO_PHSID = {
@@ -76,7 +92,15 @@ class Mapping(object):
         'TCGA-THYM': ['phs000178'],
         'TCGA-UCEC': ['phs000178'],
         'TCGA-UCS': ['phs000178'],
-        'TCGA-UVM': ['phs000178']
+        'TCGA-UVM': ['phs000178'],
+        'CGCI-BLGSP': ['phs000527', 'phs000235'],
+        'CGCI-HTMCP-CC': ['phs000528', 'phs000235'],
+        'CGCI-HTMCP-DLBCL': ['phs000529', 'phs000235'],
+        'CGCI-HTMCP-LC': ['phs000530', 'phs000235'],
+        'CGCI-MB': ['phs000531', 'phs000235'],
+        'CGCI-NHL-DLBCL': ['phs000532', 'phs000235'],
+        'CGCI-NHL-FL': ['phs000533', 'phs000235'],
+
     }
 
     def get_projects(self, phsid):
@@ -90,23 +114,22 @@ class Mapping(object):
     def get_project_level_phsid(self, project):
         '''
         Get project level phsid for a project name,
-        return None if the project doesn't exist 
+        return None if the project doesn't exist
         '''
         phsids = self.get_phsids(project)
         for phsid in phsids:
             if phsid not in self.PHSID_TREE:
-                return phsid 
-
+                return phsid
 
     def get_program_level_phsid(self, project):
         '''
         Get program level phsid for a project name,
-        return None if the project doesn't exist 
+        return None if the project doesn't exist
         '''
         phsids = self.get_phsids(project)
         for phsid in phsids:
-            if phsid in  self.PHSID_TREE:
-                return phsid 
+            if phsid in self.PHSID_TREE:
+                return phsid
 
     def get_project(self, phsid):
         '''
