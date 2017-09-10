32365dda8b6b63ad5718a07b6c80a9eea7d76572
Joe Sislow <madopal@uchicago.edu>
2017-09-10 16:08:25 +0000

    chore(setup): Bump version
    - Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cdisutils",
-    version="0.1.0",
+    version="0.1.1",
     description="Miscellaneous utilities useful for interaction with CDIS systems.",
     license="Apache",
     packages=["cdisutils"],
