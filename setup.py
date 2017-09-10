559b281ff6fba9aeb8b998fa3a455793d4e4ad91
Joe Sislow <madopal@uchicago.edu>
2017-09-10 16:25:24 +0000

    chore(setup): Add openpyxl
    - Add openpyxl to dependencies

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         'setuptools==30.1.0',
         'xmltodict==0.9.2',
         'pyOpenSSL==16.2.0',
-        'ndg-httpsclient==0.4.2'
+        'ndg-httpsclient==0.4.2',
+        'openpyxl==2.4.0'
     ],
 )
