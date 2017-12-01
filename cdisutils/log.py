071e4f95fcdeb7a4be5d8520257b9a4442beb6ab
Richard Jackson <rjackson90@gmail.com>
2017-12-01 12:31:13 -0600

    Add file handler for logs

--- cdisutils/log.py
+++ cdisutils/log.py
@@ -5,14 +5,19 @@ import sys
 
 LOGGERS = {}
 
+LOG_FORMAT=logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
 
 def get_handler():
     """Return a stdout stream handler"""
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
-    handler.setFormatter(formatter)
+    handler.setFormatter(LOG_FORMAT)
     return handler
 
+def get_file_handler(path):
+    """Return a file handler"""
+    handler = logging.FileHandler(path)
+    handler.setFormatter(LOG_FORMAT)
+    return handler
 
 def get_logger(name):
     """Return an opinionated basic logger named `name` that logs to
@@ -27,3 +32,11 @@ def get_logger(name):
             logger.addHandler(get_handler())
             logger.propagate = False
     return logger
+
+def get_file_logger(name, path):
+    """Return an opinionated basic logger named `name` that logs to
+    a file at the specified path"""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(get_file_handler(path))
+    logger.propagate = False
