1b06a31ae56eeb6249b5a589f300fba77de2c54d
Joe Sislow <madopal@uchicago.edu>
2018-01-10 00:01:39 +0000

    fix(cdisutils): Add boto and python-signpostclient to setup
    - Add boto and python-signpost client to setup, so that all
    the packages it needs are actually installed

--- setup.py
+++ setup.py
@@ -8,9 +8,15 @@ setup(
     packages=["cdisutils"],
     install_requires=[
         'setuptools==30.1.0',
+        'boto==2.36.0',
         'xmltodict==0.9.2',
         'pyOpenSSL==16.2.0',
         'openpyxl==2.4.0',
-        'ndg-httpsclient==0.4.3'
+        'ndg-httpsclient==0.4.3',
+        'python-signpostclient'
     ],
+    dependency_links = [
+        'git+ssh://git@github.com/NCI-GDC/python-signpostclient.git@ca686f55772e9a7f839b4506090e7d2bb0de5f15#egg=signpostclient',
+    ]
+
 )
