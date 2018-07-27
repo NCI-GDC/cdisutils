6ec8f1c8fa6304210fed338279c31cc453f21945
Tori Kraft <queenvkraft@gmail.com>
2018-07-27 11:18:16 -0500

    feat(dict): add method to remove keys recursively from dictionary

--- cdisutils/dictionary.py
+++ cdisutils/dictionary.py
@@ -22,3 +22,23 @@ def sort_dict(tree, remove_keys=None):
         )
     else:
         return tree
+
+def remove_keys_from_dict(tree, remove_keys):
+    """
+    Recursively remove keys from dictionary tree
+    """
+    if remove_keys is None or remove_keys == []:
+        return tree
+
+    if isinstance(tree, dict):
+        return {
+            key: remove_keys_from_dict(tree[key], remove_keys)
+            for key in tree.keys() if key not in remove_keys
+        }
+    elif isinstance(tree, list):
+        return [
+            remove_keys_from_dict(element, remove_keys)
+            for element in tree
+        ]
+    else:
+        return tree
