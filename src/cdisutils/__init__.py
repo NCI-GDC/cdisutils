9c3d6edf59d91857802f782dfa2262faf661cca4
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2024-08-12 16:32:37 -0400

    DEV-2910 update for FIPS compatibility for hashlib

--- src/cdisutils/__init__.py
+++ src/cdisutils/__init__.py
@@ -1,8 +1,11 @@
 import hashlib
+import sys
 
 
 def md5sum(iterable):
-    md5 = hashlib.md5()
+    md5 = (
+        hashlib.md5() if sys.version_info < (3, 9) else hashlib.md5(usedforsecurity=False)
+    )  # nosec
     for chunk in iterable:
         md5.update(chunk)
     return md5.hexdigest()
